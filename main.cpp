8675e59e2141feda9593f3c0eb193d1973da3877
p2rv <p2rv@github.com>
2015-02-26 00:36:02 +0800

    Refactor name function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <string.h>
-#include <stdio.h>
-#include <bits/string3.h>
 
 using namespace std;
-char crypt(const char c,const char key);
-int cryptCezar(const string source,const string key, string & destanation);
+
+int XORCipher(const string source, const string key, string * destanation);
+
 int main(int argc, char* argv[])
 {
     string plaint,key;
@@ -16,13 +14,13 @@ int main(int argc, char* argv[])
         key = argv[1];
         cin >>plaint;
         string ciphert (plaint.length(),0);
-        cryptCezar(plaint, key, ciphert);
+        XORCipher(plaint, key, & ciphert);
         cout <<"plaint: "<<plaint << endl;
         cout <<"ciphert: "<<ciphert << endl;
     }
     return 0;
 }
-int cryptCezar(const string source,const string key, string  destanation){
+int XORCipher(const string source, const string key, string * destanation){
     const int length_k=key.length();
     const int length_s=source.length();
     int j=0;
@@ -32,12 +30,7 @@ int cryptCezar(const string source,const string key, string  destanation){
             j=0;
         else
             j++;
-        destanation[i]=source[i]^key[j];
+        (*destanation)[i]=source[i]^key[j];
     }
     return 0;
-}
-
-char crypt(const char  c,const char key)
-{
-    return (c)^(key);
 }
