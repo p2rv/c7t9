122e57a2d7ea7e90a28f5c17f6f08250917a37e7
p2rv <p2rv@github.com>
2015-02-26 01:05:50 +0800

    Fix bug and add description algoritm

--- main.cpp
+++ main.cpp
@@ -7,23 +7,26 @@ int XORCipher(const string source, const string key, string * destanation);
 int main(int argc, char* argv[])
 {
     string plaint,key;
-    if (argc==1)
-        cout <<"не указан ключ";
-    else
-    {
+    if (argc==2)
         key = argv[1];
-        cin >>plaint;
-        string ciphert (plaint.length(),0);
-        XORCipher(plaint, key, & ciphert);
+    cin >>plaint;
+    string ciphert (plaint.length(),0);
+    if (XORCipher(plaint, key, & ciphert)==0){
+        cout <<"Message is successfully encrypted "<< endl;
         cout <<"plaint: "<<plaint << endl;
         cout <<"ciphert: "<<ciphert << endl;
     }
+    else
+        cout <<"Error: key is not defined" << endl;
+
     return 0;
 }
 int XORCipher(const string source, const string key, string * destanation){
     const int length_k=key.length();
     const int length_s=source.length();
     int j=0;
+    if(length_k==0)
+        return -1;
     for(int i=0;i<length_s;i++)
     {
         if(j>length_k)
